d0dfded6484733050e79b0dcab51aec03583b2e3
Highdrien <adrien.zabban@outlook.fr>
2023-12-13 21:36:58 +0100

    first implementation

--- src/dataloader/process_getitem_index.py
+++ src/dataloader/process_getitem_index.py
@@ -16,7 +16,7 @@ def process_text_index(files: Files,
     si ignore_first_index = t, récupère seulement les élements tq ipu_id > t
     """
     text_index: List[Tuple[str, int, float]] = []
-    all_label = List[bool] = []
+    all_label: List[bool] = []
     
     for file in files.keys():
         filepath = os.path.join(data_path, 'transcr', file + '_merge.csv')
@@ -35,32 +35,54 @@ def process_text_index(files: Files,
 
 def process_index_video(files: Files,
                         data_path: str,
-                        text_index: Text_Index):
+                        text_index: Text_Index,
+                        step_frame: Optional[float]=0.040):
+    
     data_path = os.path.join(data_path, 'video', 'openface')
     file_list = os.listdir(data_path)
     output = []
 
     for file in files.keys():
         speakers = files[file]
-        dataset = 'chesse'
+        dataset = 'cheese'
         filename = f"{dataset}_{file}_{speakers[0]}.csv"
 
         if filename not in file_list:
             dataset = 'paco'
         
         for speaker in speakers:
-            filename = f"{dataset}_{file}_{speaker}.csv"
-            
+            filename = os.path.join(data_path, f"{dataset}_{file}_{speaker}.csv")
+            ic(filename)
+
             df = pd.read_csv(filename)
-            # frame_id = df['frame'].tolist()
-            timestamp = df['timestamp'].tolist()
+            frame_id = df['frame'].tolist()
+            timestamp = df[' timestamp'].tolist()
+            max_timestamp = max(timestamp)
             del df
 
-            for _, _, time in text_index:
-                #TODO: match les time de text index et les timestamp
-                pass
+            for i, (_, _, time) in enumerate(text_index):
+                if time < max_timestamp:
+                    frame_time = step_frame * (time // step_frame)
+                    frame_time = round(frame_time, 3)
+                    if frame_time not in timestamp:
+                        print(f"attention erreur: {file = }, {time = }, {frame_time = }")
+                        exit()
+                    index = timestamp.index(frame_time) + 1 # car les images commance à 1
+                    
+                
 
 
 
 
-
+if __name__ == '__main__':
+    from icecream import ic
+    files = {'JAEA': ['JA', 'EA'],
+             'JSCL': ['JS', 'CL'],
+             'LPMA': ['LP', 'MA'],
+             'MDAD': ['MD', 'AD'],
+             'RPABN': ['RPA', 'BN']}
+    data_path = 'data'
+    text_index, all_label = process_text_index(files=files, data_path=data_path)
+    # ic(text_index)
+    # ic(all_label)
+    process_index_video(files=files, data_path=data_path, text_index=text_index)
