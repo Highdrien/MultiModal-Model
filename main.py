44fcd5a9967c2791a989dfdf4fb05b0d19d97c3a
Highdrien <adrien.zabban@outlook.fr>
2024-01-25 16:56:14 +0100

    chnage the way to test the latefusion and add choise in parsers

--- main.py
+++ main.py
@@ -9,16 +9,8 @@ from utils import utils
 
 import os
 
-os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
-
-
-
-IMPLEMENTED = ['train', 'test', 'baseline']
 
 def main(options: dict) -> None:
-
-    if options['mode'] not in IMPLEMENTED:
-        raise ValueError(f"Expected mode must in {IMPLEMENTED} but found {options['mode']}")
     
     config = load_config(options['config_path'])
 
@@ -29,21 +21,23 @@ def main(options: dict) -> None:
         train(config)
     
     if options['mode'] == 'test':
-        if options['path'] is None and utils.is_model_likelihood(config):
-            config.task = 'likelihood'
-            logging_path = train_logger(config=config, write_train_log=False)
-            config.task = 'multi'
-            ic(logging_path)
-            test(config=config,
-                 logging_path=logging_path)
         
-        elif options['path'] is None:
-            raise ValueError(f'you must specify the path of the experiment that you want to test, or use a likelihood config')
+        if options['path'] is None:
+            raise ValueError(f'you must specify the path of the experiment that you want to test')
+        
+        config = load_config(find_config(experiment_path=options['path']))
+        ic(config)
+        test(config, logging_path=options['path'])
+    
+    if options['mode'] == 'latefusion':
+        config.task = 'likelihood'
+        config.model.multi.likelihood = True
+        logging_path = train_logger(config=config, write_train_log=False)
+        config.task = 'multi'
+        ic(logging_path)
+        test(config=config,
+             logging_path=logging_path)
 
-        else:
-            config = load_config(find_config(experiment_path=options['path']))
-            ic(config)
-            test(config, logging_path=options['path'])
     
     if options['mode'] == 'baseline':
         config = load_config(options['config_path'])
@@ -54,12 +48,14 @@ if __name__ == "__main__":
 
     # Options
     parser.add_argument('--mode', '-m', default=None, type=str,
-                        help="choose a mode between 'train', 'data'")
+                        choices=['train', 'test', 'latefusion'],
+                        help="choose a mode between 'train', 'test'")
     parser.add_argument('--config_path', '-c', default=os.path.join('config', 'config.yaml'),
                         type=str, help="path to config (for training)")
     parser.add_argument('--path', '-p', type=str,
                         help="experiment path (for test, prediction or generate)")
     parser.add_argument('--task', '-t', type=str, default=None,
+                        choices=['text', 'video', 'audio', 'multi'],
                         help="task for model (will overwrite the config) for trainning")
 
     args = parser.parse_args()
